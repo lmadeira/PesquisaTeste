fccff3efe8a82d0eb66dbae57a211f361f5f5cae
Leonardo Madeira <leomad@Leo-Madeira-Mac.local>
2012-06-18 12:17:47 -0300

    teste

--- Resources/Custom/UI/Menu.js
+++ Resources/Custom/UI/Menu.js
@@ -11,7 +11,7 @@ function Menu(title){
                 textAlign:'center',
                 width:'auto',
                 height:'auto',
-                bottom:20,
+                bottom:40,
                 font:{fontFamily:'Arial',fontWeight:'bold',fontSize:24},
                 color:'#000000'
             });
